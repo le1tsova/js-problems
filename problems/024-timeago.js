40acef4e4e6f5b7fb9bb17e640ef44467353951f
Kate Levantsova <kate.levantsova@gmail.com>
2019-08-23 16:46:36 +0300

    Добавила константы, убрала проверки

--- problems/024-timeago.js
+++ problems/024-timeago.js
@@ -14,26 +14,30 @@
  * @param {number} seconds
  * @returns {string}
  */
+
+const TWO_WEEKS = 1209600;
+const FOUR_WEEKS = 2419200;
+
 function timeago(seconds) {
     if (seconds < 10) {
         return "just now";
     }
-    if (seconds < 50) {
-        return Math.floor(seconds / 10) + "0 seconds ago";
+    if (seconds < 60) {
+        return String(seconds / 10)[0] + "0 seconds ago";
     }
     if (seconds < 120) {
         return "1 minute ago";
     }
-    if (seconds >= 120 && seconds < 1800) {
+    if (seconds < 1800) {
         return Math.floor(seconds / 60) + " minutes ago";
     }
-    if (seconds >= 1800 && seconds < 3600) {
+    if (seconds < 3600) {
         return "30 minutes ago";
     }
-    if (seconds >= 3600 && seconds < 7200) {
+    if (seconds < 7200) {
         return "1 hour ago";
     }
-    if (seconds >= 7200 && seconds < 86400) {
+    if (seconds < 86400) {
         if (Math.floor(seconds / 3600) <= 12) {
             return Math.floor(seconds / 3600) + " hours ago";
         } else {
@@ -41,16 +45,16 @@ function timeago(seconds) {
         }
     }
 
-    if (seconds >= 86400 && seconds < 172800) {
+    if (seconds < 172800) {
         return "1 day ago";
     }
-    if (seconds >= 172800 && seconds < 604800) {
+    if (seconds < 604800) {
         return "a few days ago";
     }
-    if (seconds >= 604800 && seconds < 1209600) {
+    if (seconds < TWO_WEEKS) {
         return "1 week ago";
     }
-    if (seconds >= 1209600 && seconds < 2419200) {
+    if (seconds < FOUR_WEEKS) {
         return Math.floor(seconds / 3600 / 24 / 7) + " weeks ago";
     }
     return "undefined";
